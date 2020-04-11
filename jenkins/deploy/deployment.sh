c4f6da30537b62c7b1661ae15cbd2f64e68fb748
born <born@ubuntu_foo.org>
2020-04-11 04:26:05 -0700

    initial setup

--- jenkins/deploy/deployment.sh
+++ jenkins/deploy/deployment.sh
@@ -3,12 +3,12 @@
 # $3 ... repository
 # $4 ... deployment name
 
-microk8s.helm3 install $4 helm/
+microk8s.helm install $4 helm/
 if [ $? -eq 0 ]
 then
   echo "Successfully created deployment"
 else
   echo "Failed to create deployment..."
   echo "Try to update deployment..."
-  microk8s.helm3 upgrade $4 helm/
+  microk8s.helm upgrade $4 helm/
 fi
