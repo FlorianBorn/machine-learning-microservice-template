ac85969d3a07f8e44cff7fa918824e4d95ccab62
born <born@ubuntu.foo>
2020-04-24 14:37:50 -0700

    use startup to init the app; add logger; load config from yaml

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # from https://fastapi.tiangolo.com/tutorial/first-steps/
+
+# Template Imports
 import sys
 sys.path.append("source")
 from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
 from source.model.mdl import Model
+from source.logging import Logger
 import pandas as pd
 import pickle as pkl
+import yaml
 
 
 
 app = FastAPI()
-
-# load model
-with open("model_bin/model.pkl", "rb") as fp:
-    model = pkl.load(fp)
 
 # define Request-Parameter
 class PredictionRequest(BaseModel):
@@ -30,6 +30,19 @@ class ProbaPredictionResponse(BaseModel):
     class_names: List[str]
     probabilities: List[float]
 
+@app.on_event("startup")
+async def startup():
+    #await print("startup!")
+    with open("config.yaml", "r") as fp:
+        app.config = yaml.load(fp)
+    with open("model_bin/model.pkl", "rb") as fp:
+        app.model = pkl.load(fp)
+    app.logger = Logger(client_url=app.config['db_url'])
+
+@app.on_event("shutdown")
+async def shutdown():
+    print(foo)
+    print("shutdown!")
 
 @app.get("/api/hello-world")
 def read_root():
@@ -41,17 +54,18 @@ def predict(request: List[PredictionRequest]):
     request can contain 1 or more samples
     '''
     X = prepare_request(request)
-    predictions = model.predict(X)
+    predictions = app.model.predict(X)
     response = [{"class_name": str(p)} for p in predictions] # convert prediction (array) to a list of dicts
+    app.logger.emit_many(response)
     return response
 
 @app.post("/api/proba_predictions", response_model=List[ProbaPredictionResponse])
 def predict_proba(request: List[PredictionRequest]):
     X = prepare_request(request)
-    predictions = model.predict_proba(X).tolist()
+    predictions = app.model.predict_proba(X).tolist()
 
     # prepare response
-    classes = model.classes
+    classes = app.model.classes
     response = []
     for p in predictions:
         d = {"class_names": classes, "probabilities": p}
