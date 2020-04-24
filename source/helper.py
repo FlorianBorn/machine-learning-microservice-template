92091a4a0bfe3e0140a5a6f495b11460ac5a0a22
born <born@ubuntu.foo>
2020-04-24 16:53:29 -0700

    chore: add helper function to load the config.yaml

--- source/helper.py
+++ source/helper.py
@@ -1,6 +1,15 @@
 from source.data.data_loader import DataLoader
 from source.model.mdl import Model
 from source.config import default_model_path
+from pathlib import Path
+import yaml
+
+
+def load_config():
+    config_path = Path(__file__).resolve().parents[1]
+    with open(str(config_path / "config.yaml"), "r") as fp:
+        config = yaml.load(fp)
+    return config    
 
 def train_and_store_model():
     df_data = DataLoader().load_data()
