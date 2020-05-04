972ba6aaf2f5ad2b535853385482f2427967d2fb
FlorianBorn <29048675+FlorianBorn@users.noreply.github.com>
2020-05-04 15:11:21 -0700

    chore: read image name and image tag from config.yaml

--- dockerfiles/runtime-image/build.sh
+++ dockerfiles/runtime-image/build.sh
@@ -1,3 +1,5 @@
 cd ../..
-docker build --build-arg docker_gid=$(stat -c '%g' /var/run/docker.sock) -t ml_template_runtime_env -f dockerfiles/runtime-image/Dockerfile .
+image_name=$(grep 'runtime_image_name' config.yaml | awk '{ print $2 }')
+image_tag=$(grep 'runtime_image_tag' config.yaml | awk '{ print $2 }')
+docker build --build-arg docker_gid=$(stat -c '%g' /var/run/docker.sock) -t $image_name:$image_tag -f dockerfiles/runtime-image/Dockerfile .
 cd dockerfiles/runtime-image
