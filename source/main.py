c4a08b311806b1b9f005a9c8d43b99405bac6832
FlorianBorn <29048675+FlorianBorn@users.noreply.github.com>
2020-05-31 19:12:18 -0700

    feat: integrate fluentd logger

--- source/main.py
+++ source/main.py
@@ -7,7 +7,8 @@ from typing import List
 from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel
 from source.model.mdl import Model
-from source.logging.logging import Logger
+from source.logging.mongodb_logger import MongoDBLogger
+from source.logging.fluentd_logger import FluentdLogger
 import pandas as pd
 import pickle as pkl
 import yaml
@@ -25,12 +26,17 @@ async def startup():
     with open("model_bin/model.pkl", "rb") as fp:
         app.state.model = pkl.load(fp)
     if app.state.config['enable_logging'] == True:
-        app.state.logger = Logger(client_url=app.state.config['mongo_db_url'],
+        app.state.mongodb_logger = MongoDBLogger(
+            client_url=app.state.config['mongo_db_url'],
             port=app.state.config['mongo_db_port'],
             db_name=app.state.config['mongo_db_name'],
             collection_name=app.state.config['logging_collection_name'],
             timezone=app.state.config['timezone'])
-
+    if app.state.config['enable_fluentd_logging'] == True:
+        app.state.fluentd_logger = FluentdLogger(
+            fluentd_url=app.state.config['fluentd_url'],
+            fluentd_port=app.state.config['fluentd_port'],
+            fluentd_base_log_name=app.state.config['fluentd_base_log_name'])
 
 @app.on_event("shutdown")
 async def shutdown():
